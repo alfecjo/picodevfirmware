afc38590fa7ddfc89f3aff879f36d7700a5ff3e5
Antonio Almeida - PCD <alfecjo@hotmail.com>
2025-05-28 00:37:13 -0300

    Delete project011 directory

--- project011/setenv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Carrega variáveis do .env e roda o CMake
-
-set -a             # Exporta todas as variáveis
-source .env        # Lê o arquivo .env
-set +a             # Para de exportar automaticamente
-
-cmake -S . -B build
-cmake --build build
